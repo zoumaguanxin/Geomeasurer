5df2d1fc440714b11f2e1ef7a76e88aec34531e5
zoumaguanxin <398044653@qq.com>
2017-12-15 11:54:00 +0800

    first commit

---
+++ geomeasurer/sensor/scan_data.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017, <copyright holder> <email>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY <copyright holder> <email> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <copyright holder> <email> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+#ifndef SCAN_DATA_H
+#define SCAN_DATA_H
+#include <boost/concept_check.hpp>
+#include<vector>
+#include<pcl/common/common.h>
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+typedef pcl::PointXYZ point3d;
+
+namespace geomeasurer
+{
+  namespace sensor
+  {
+
+struct rangeData{
+  double angle_increment;
+  double angle_min;
+  double angle_max;
+ std:: vector<double> ranges;
+};
+
+ PointCloud fromRangeData(const rangeData &ranges_data);
+}
+}
+
+#endif // SCAN_DATA_H
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "assert.h"
+using namespace std;
+int main(int argc, char **argv) {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+    assert(true);
+}
